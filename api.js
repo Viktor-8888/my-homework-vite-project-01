57889cdbf1f1e7737a19c4d90e83e2d96f017f91
Viktor-8888 <ahmatov@ukr.net>
2026-04-27 14:42:21 +0000

    Deploying to gh-pages from @ Viktor-8888/my-homework-vite-project-01@7dd7a27fa83cae597777bccb92179bc9cd08cfba 🚀

---
+++ api.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{a as o}from"./assets/vendor-Cr2dB-OS.js";console.dir(o);
+//# sourceMappingURL=api.js.map
